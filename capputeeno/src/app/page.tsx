beefb0bbba229112afc7da891504e08d4e8b3a69
Rogeriobg <studiorgonline@gmail.com>
2023-10-17 18:23:47 -0300

    criando pasta icons

--- capputeeno/src/app/page.tsx
+++ capputeeno/src/app/page.tsx
@@ -1,10 +1,24 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client"
+
+import { DefaultPageLayout } from '@/components/default-page-layout'
+import { FilterBar } from '@/components/filter-bar'
+import { ProductsList } from '@/components/products-list'
+import { styled } from 'styled-components'
+
+const PageWrapper = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
 
-    </main>
+  return (
+    <DefaultPageLayout>
+      <PageWrapper>
+        <FilterBar />
+        <ProductsList />
+      </PageWrapper>
+    </DefaultPageLayout>
   )
 }
--- capputeeno/src/components/primary-input.tsx
+++ capputeeno/src/components/primary-input.tsx
@@ -1,38 +1,56 @@
+import { InputHTMLAttributes } from "react";
 import { styled } from "styled-components";
 import { SearchIcon } from "./search-icon";
-import { InputHTMLAttributes } from "react";
 
 
 export const PrimaryInput = styled.input`
-width:352px;
-border-radius:8px;
-boder:none;
-background-color: var(--bg-secondary);
-padding:10px 16px;
+    width: 100%;
+    border-radius: 8px;
+    border: none;
+    padding: 10px 16px;
 
-font-family:inherit;
-font-weight:400;
-font-size:14px;
-line-height:22px;
-color: var(--text-dark);
+    background-color: var(--bg-secondary);
+
+    font-family: inherit;
+    font-weight: 400;
+    font-size: 12px;
+    line-height: 20px;
+    color: var(--text-dark);
+
+    @media(min-width: ${props => props.theme.desktopBreakpoint}){
+        font-size: 14px;
+        line-height: 22px;
+    }
 `
 
 const InputContainer = styled.div`
-position:relative;
-width:352px;
-svg{
-    position:absolute;
-    right:20px;
-    top:40%;
-    tranform:tranlaterY(-50%);
-}
+    position: relative;
+    width: 250px;
+
+    svg {
+        position: absolute;
+        right: 20px;
+        top: 50%;
+        transform: translateY(-50%);
+    }
+
+    @media(min-width: ${props => props.theme.desktopBreakpoint}){
+        width: 352px;
+    }
 `
 
-interface InputProps extends InputHTMLAttributes<HTMLInputElement> { }
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+    value: string,
+    handleChange: (value: string) => void
+}
+
 export function PrimaryInputWSearchIcon(props: InputProps) {
     return (
         <InputContainer>
-            <PrimaryInput {...props} />
+            <PrimaryInput
+                onChange={(event) => props.handleChange(event.target.value)}
+                {...props}
+            />
             <SearchIcon />
         </InputContainer>
     )
